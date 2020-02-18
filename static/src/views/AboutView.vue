cd751c1c9f00790d09b1f4997a89b1ff53b2ee18
QuickSoOrt <brunorafaelfariameira@gmail.com>
2020-02-18 13:29:50 +0000

    solved errors

--- static/src/views/AboutView.vue
+++ static/src/views/AboutView.vue
@@ -1,57 +1,24 @@
 <template>
-	<div>
-		<div id='imgsCarousel' class='carousel slide' data-ride='carousel'>
-			
-			<div class='carousel-inner'>
-				<div v-bind:class="returnClass(index)" v-for='(img, index) in images'>
+	
+	<section id="events" class="pb-5">
+		
+		<div id="events_header" class="container">
 
-					<img v-bind:src="'/static/public/assets/cs/' + img" class='card-img-top d-block w-100'>
-
-				</div>
-
-			</div>
-
-			<a class="carousel-control-prev" href="#imgsCarousel" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#imgsCarousel" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+			<div class="row mt-5">
+                <div class="col-12">
+                    <h1 class='m-about-title'><strong><span>Eventos</span></strong></h1>
+                </div>
+            </div>
 
 		</div>
 
-		<p>&nbsp;</p>
+		<div id="events_content" class="container mt-5">
 
-		<div>
-
-			<p class='m-about-text'>
-				<span>O IEEE Computer Society é a maior sociedade internacional na área das Tecnologias da informação, dedicada a contribuir para o progresso desta área, através do aperfeiçoamento das capacidades da comunidade informática.
-				</span>
-			</p>
-
-			<p class='m-about-text'>
-				<span>Como membros deste student chapter no IEEE-IST, temos o objetivo de dar aos estudantes do IST ferramentas para o desenvolvimento de capacidades, que poderão ajudar no seu crescimento enquanto profissionais na área das Tecnologias da Informação, propondo-lhes iniciativas que o promovam.
-				</span>
-			</p>
-
-			<p class='m-about-text'>
-				<span>Com esta finalidade, temos vindo a oferecer workshops não só teóricos como práticos,de forma a aprofundar o conhecimento dos alunos de Informática; realizar competições dentro do IST, para criar e desenvolver o interesse dos estudantes e estabelecer uma plataforma que permita aos alunos pôr em prática as capacidades desenvolvidas.
-				</span>
-			</p>
 
 		</div>
 
-		<p>&nbsp;</p>
-		<h1 class='m-about-title'>
-			<strong><span>Eventos</span></strong>
-		</h1>
-		<p>&nbsp;</p>
+	</section>
 
-		<events-carousel/>
-
-	</div>
 </template>
 
 <script lang='ts'>
@@ -72,7 +39,6 @@
 
 			return "m-item carousel-item";
 		}
-
 	}
 
 </script>
@@ -80,25 +46,13 @@
 <style scoped>
 	
 	.m-about-title {
-		text-align: center;
-		color: #808080;
-	}
+        text-align: center;
+        color: #808080;
+    }
 
-	.m-about-text {
-		text-align: justify;
-		color: #808080;
-		margin-left: 10%;
-		margin-right: 10%;
-	}
-
-	#imgsCarousel {
-		margin-left: 10%;
-		margin-right: 10%;
-	}
-
-	.carousel-inner > .m-item > img {
-		width:640px;
-		height:360px;
-	}
+    .m-about-text {
+        text-align: center;
+        color: #808080;
+    }
 
 </style>
--- static/src/views/MembersView.vue
+++ static/src/views/MembersView.vue
@@ -1,20 +1,124 @@
 <template>
-	<div>
-		oola
-	</div>
+
+	<section id="team" class="pb-5">
+
+        <div id=team_header class="container">
+
+            <div class="row mt-5">
+                <div class="col-12">
+                    <h1 class='m-about-title'><strong><span>Membros</span></strong></h1>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-12">
+                    <p class='m-about-text'>
+                        <span>A nossa equipa é composta por alunos de todos os cursos de Engenharia do Instituto Superior Técnico</span>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+		<div id="team_content" class="container mt-5">
+            
+            <div id="department" class="row mt-5" v-for='dep in departments'>
+        		<div id="department_title" class="col-12 mb-4">
+                    <h5 class="h4">{{dep.name}}</h5>
+                </div>
+
+                <div id="department_member" class="col-xs-12 col-sm-6 col-md-4" v-for='mem in dep.members'>
+        				<member-card :member='mem' />
+        		</div>
+            
+            </div>
+	    
+        </div>
+    
+    </section>
+
 </template>
 
 <script lang='ts'>
 
 	import { Vue, Component } from 'vue-property-decorator';
 
-	@Component({})
-	export default class MembersView extends Vue {
+	import MemberCard from '../components/MemberCard.vue';
 
+	interface Member {
+		name: string;
+		role: string;
+		description: string;
+		image: string;
+	}
+
+    interface Department {
+        name: string;
+        members: Member[];
+    }
+
+	@Component({ components: { 'member-card': MemberCard, }, })
+	export default class TeamView extends Vue {
+
+        public images: string[] = ['team.jpg', 'header.jpg', 'team1819.png'];
+
+		public departments : Department[] = [
+            {'name': 'Board', 'members' : 
+                [
+                {'name':'Joana Nápoles', 'role':'Chair', 'description': 'A Joana entrou para o IEEE-IST SB como colaboradora do projeto Younique no início do ano de 2018. No ano de 2018/2019 entrou para a EMBS-IST como Innovation Manager, cargo que ocupa atualmente, como membro ativo na direção. Neste cargo tem a oportunidade de garantir que cada iniciativa e departamento cresça da melhor forma, contribuindo para a motivação de cada equipa e o sucesso dos alunos da nossa instituição. As principais características que a movem são a sua paixão por ajudar o próximo e a sua ambição para fazer o seu melhor em tudo.', 'image': 'JoanaNapoles2.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'}
+                ]
+            },
+            {'name': 'Board', 'members' : 
+                [
+                {'name':'Joana Nápoles', 'role':'Chair', 'description': 'A Joana entrou para o IEEE-IST SB como colaboradora do projeto Younique no início do ano de 2018. No ano de 2018/2019 entrou para a EMBS-IST como Innovation Manager, cargo que ocupa atualmente, como membro ativo na direção. Neste cargo tem a oportunidade de garantir que cada iniciativa e departamento cresça da melhor forma, contribuindo para a motivação de cada equipa e o sucesso dos alunos da nossa instituição. As principais características que a movem são a sua paixão por ajudar o próximo e a sua ambição para fazer o seu melhor em tudo.', 'image': 'JoanaNapoles2.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'}
+                ]
+            },
+            {'name': 'Board', 'members' : 
+                [
+                {'name':'Joana Nápoles', 'role':'Chair', 'description': 'A Joana entrou para o IEEE-IST SB como colaboradora do projeto Younique no início do ano de 2018. No ano de 2018/2019 entrou para a EMBS-IST como Innovation Manager, cargo que ocupa atualmente, como membro ativo na direção. Neste cargo tem a oportunidade de garantir que cada iniciativa e departamento cresça da melhor forma, contribuindo para a motivação de cada equipa e o sucesso dos alunos da nossa instituição. As principais características que a movem são a sua paixão por ajudar o próximo e a sua ambição para fazer o seu melhor em tudo.', 'image': 'JoanaNapoles2.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'}
+                ]
+            },
+            {'name': 'Board', 'members' : 
+                [
+                {'name':'Joana Nápoles', 'role':'Chair', 'description': 'A Joana entrou para o IEEE-IST SB como colaboradora do projeto Younique no início do ano de 2018. No ano de 2018/2019 entrou para a EMBS-IST como Innovation Manager, cargo que ocupa atualmente, como membro ativo na direção. Neste cargo tem a oportunidade de garantir que cada iniciativa e departamento cresça da melhor forma, contribuindo para a motivação de cada equipa e o sucesso dos alunos da nossa instituição. As principais características que a movem são a sua paixão por ajudar o próximo e a sua ambição para fazer o seu melhor em tudo.', 'image': 'JoanaNapoles2.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'},
+                {'name':'Filipa Rocha', 'role':'Vice-Chair', 'description': 'Hi, I am asdasdaed', 'image':'FilipaRocha.jpg'}
+                ]
+            }
+        ];
+
+        public returnClass(idx) : string {
+            if (idx == 0)
+                return "m-item carousel-item active";
+            
+            return "m-item carousel-item";
+        }
  	}
 
 </script>
 
 <style scoped>
-	
+        
+    #department_title {
+        text-align: center;
+        color: #007b5e;
+        text-transform: uppercase;
+    }
+
+    .m-about-title {
+        text-align: center;
+        color: #808080;
+    }
+
+    .m-about-text {
+        text-align: center;
+        color: #808080;
+    }
+
 </style>
